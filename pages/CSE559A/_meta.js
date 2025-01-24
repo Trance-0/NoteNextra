84594916dd6f7d67b54a8ff078b5e1afffe56682
Zheyuan Wu <60459821+Trance-0@users.noreply.github.com>
2025-01-24 11:48:20 -0600

    update

--- pages/CSE559A/_meta.js
+++ pages/CSE559A/_meta.js
@@ -3,6 +3,8 @@ export default {
     "---":{
         type: 'separator'
     },
-    CSE559A_L1: "Cmputer Vision (Lecture 1)",
-    CSE559A_L2: "Cmputer Vision (Lecture 2)",
+    CSE559A_L1: "Computer Vision (Lecture 1)",
+    CSE559A_L2: "Computer Vision (Lecture 2)",
+    CSE559A_L3: "Computer Vision (Lecture 3)",
+    CSE559A_L4: "Computer Vision (Lecture 4)",
 }
