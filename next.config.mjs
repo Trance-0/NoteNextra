e92443b36283b00c2a2504949c7403ad057397f7
Trance-0 <60459821+Trance-0@users.noreply.github.com>
2025-02-11 21:10:47 -0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -13,7 +13,11 @@ const withNextra = nextra({
   }
 })
 
-export default withNextra()
+export default withNextra({
+  experimental: {
+    // optimize memory usage: https://nextjs.org/docs/app/building-your-application/optimizing/memory-usage
+    webpackMemoryOptimizations: true,
+  },})
 
 // If you have other Next.js configurations, you can pass them as the parameter:
 // export default withNextra({ /* other next.js config */ })
