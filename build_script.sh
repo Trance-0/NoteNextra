573ee8434b9a45c0196356422a1ad9160513f392
Zheyuan Wu <60459821+Trance-0@users.noreply.github.com>
2025-06-16 19:42:37 -0500

    update timestamp

---
+++ build_script.sh
@@ -0,0 +1,1 @@
+npm run build --max_old_space_size=8192
--- pages/_meta.js
+++ pages/_meta.js
@@ -18,45 +18,85 @@ export default {
       },
       theme:{
         sidebar: false,
+        timestamp: true,
       }
     },
     Math3200: {
       type: 'page',
+      theme:{
+        timestamp: true,
+      }
     },
     Math429:{
       type: 'page',
+      theme:{
+        timestamp: true,
+      }
     },
     Math4111: {
       type: 'page',
+      theme:{
+        timestamp: true,
+      }
     },
     Math4121: {
       type: 'page',
+      theme:{
+        timestamp: true,
+      }
     },
     Math416: {
       type: 'page',
+      theme:{
+        timestamp: true,
+      }
     },
     Math401: {
       type: 'page',
+      theme:{
+        timestamp: true,
+      }
     },
     CSE332S: {
       type: 'page',
+      theme:{
+        timestamp: true,
+      }
     },
     CSE347: {
       type: 'page',
+      theme:{
+        timestamp: true,
+      }
     },
     CSE442T: {
       type: 'page',
+      theme:{
+        timestamp: true,
+      }
     },
     CSE559A: {
       type: 'page',
+      theme:{
+        timestamp: true,
+      }
     },
     Swap: {
-      display: 'hidden'
+      display: 'hidden',
+      theme:{
+        timestamp: true,
+      }
     },
     about: {
-        display: 'hidden'
+      display: 'hidden',
+      theme:{
+          timestamp: true,
+      }
     },
     contact: {
-        display: 'hidden'
+      display: 'hidden',
+      theme:{
+          timestamp: true,
+      }
     }
   }
