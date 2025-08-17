d75813d8720e19ac8d26a9ef1262462a6544e0ac
Trance-0 <60459821+Trance-0@users.noreply.github.com>
2025-08-17 11:48:10 +1000

    update postbuild script

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+export default {
+    siteUrl: 'https://notenextra.trance-0.com',
+    changefreq: 'weekly',
+    priority: '0.5',
+    generateIndexSitemap: false,
+    exclude: ['/icon.svg']
+  }
