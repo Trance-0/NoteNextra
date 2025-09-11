533ea36b37775a35956a9163d8cb67dbd2928b3f
Zheyuan Wu <60459821+Trance-0@users.noreply.github.com>
2025-09-11 12:47:03 -0500

    updates

--- content/CSE510/_meta.js
+++ content/CSE510/_meta.js
@@ -6,4 +6,7 @@ export default {
     CSE510_L1: "CSE510 Deep Reinforcement Learning (Lecture 1)",
     CSE510_L2: "CSE510 Deep Reinforcement Learning (Lecture 2)",
     CSE510_L3: "CSE510 Deep Reinforcement Learning (Lecture 3)",
+    CSE510_L4: "CSE510 Deep Reinforcement Learning (Lecture 4)",
+    CSE510_L5: "CSE510 Deep Reinforcement Learning (Lecture 5)",
+    CSE510_L6: "CSE510 Deep Reinforcement Learning (Lecture 6)",
 }
--- content/CSE5313/_meta.js
+++ content/CSE5313/_meta.js
@@ -7,4 +7,6 @@ export default {
     CSE5313_L2: "CSE5313 Coding and information theory for data science (Lecture 2)",
     CSE5313_L3: "CSE5313 Coding and information theory for data science (Lecture 3)",
     CSE5313_L4: "CSE5313 Coding and information theory for data science (Lecture 4)",
+    CSE5313_L5: "CSE5313 Coding and information theory for data science (Lecture 5)",
+    CSE5313_L6: "CSE5313 Coding and information theory for data science (Lecture 6)",
 }
