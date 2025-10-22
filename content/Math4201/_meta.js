81a1425f873fb546988d50dc41569b04d3ce7571
Zheyuan Wu <60459821+Trance-0@users.noreply.github.com>
2025-10-22 14:43:13 -0500

    updates?
    final end of nextra.
    
    Compilation.hooks.processAssets
    RangeError: Invalid string length

--- content/Math4201/_meta.js
+++ content/Math4201/_meta.js
@@ -26,4 +26,5 @@ export default {
     Math4201_L20: "Topology I (Lecture 20)",
     Math4201_L21: "Topology I (Lecture 21)",
     Math4201_L22: "Topology I (Lecture 22)",
+    Math4201_L23: "Topology I (Lecture 23)",
 }
