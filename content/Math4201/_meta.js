78d6decd106a618b93428e68c23fb96cda191cc0
Zheyuan Wu <60459821+Trance-0@users.noreply.github.com>
2025-10-29 11:51:13 -0500

    updates

--- content/Math4201/_meta.js
+++ content/Math4201/_meta.js
@@ -29,4 +29,5 @@ export default {
     Math4201_L23: "Topology I (Lecture 23)",
     Math4201_L24: "Topology I (Lecture 24)",
     Math4201_L25: "Topology I (Lecture 25)",
+    Math4201_L26: "Topology I (Lecture 26)",
 }
