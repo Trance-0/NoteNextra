2a094032b7a54795bef0acdd96daa499f3af410d
Trance-0 <60459821+Trance-0@users.noreply.github.com>
2025-11-25 12:46:41 -0600

    update?

--- content/CSE5313/_meta.js
+++ content/CSE5313/_meta.js
@@ -28,4 +28,5 @@ export default {
     CSE5313_L22: "CSE5313 Coding and information theory for data science (Lecture 22)",
     CSE5313_L23: "CSE5313 Coding and information theory for data science (Lecture 23)",
     CSE5313_L24: "CSE5313 Coding and information theory for data science (Lecture 24)",
+    CSE5313_L25: "CSE5313 Coding and information theory for data science (Lecture 25)",
 }
