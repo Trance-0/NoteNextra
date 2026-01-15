33e5d8ec910ae265f3a40019196f9d907d9d9911
Trance-0 <60459821+Trance-0@users.noreply.github.com>
2026-01-15 12:06:21 -0600

    updates

--- content/CSE4303/_meta.js
+++ content/CSE4303/_meta.js
@@ -4,4 +4,5 @@ export default {
         type: 'separator'
     },
     CSE4303_L1: "Introduction to Computer Security (Lecture 1)",
+    CSE4303_L2: "Introduction to Computer Security (Lecture 2)",
 }
