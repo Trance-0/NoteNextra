a4d70248848c4987bf8c1553899f7d87332eb49a
Zheyuan Wu <60459821+Trance-0@users.noreply.github.com>
2026-01-20 10:57:28 -0600

    Update _meta.js

--- content/Math401/_meta.js
+++ content/Math401/_meta.js
@@ -8,4 +8,5 @@ export default {
     Math401_N3: "Math 401, Notes 3",
     Freiwald_summer: "Math 401, Summer 2025: Freiwald research project notes",
     Extending_thesis: "Math 401, Fall 2025: Thesis notes",
+    Math401_H1: "Math 401, Final Thesis"
 }
