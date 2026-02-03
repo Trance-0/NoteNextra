8f2e613b36406bf7d1b330896968693f6e6c980b
Zheyuan Wu <60459821+Trance-0@users.noreply.github.com>
2026-02-03 14:59:32 -0600

    fix errors and update news

--- content/CSE4303/_meta.js
+++ content/CSE4303/_meta.js
@@ -8,4 +8,6 @@ export default {
     CSE4303_L3: "Introduction to Computer Security (Lecture 3)",
     CSE4303_L4: "Introduction to Computer Security (Lecture 4)",
     CSE4303_L5: "Introduction to Computer Security (Lecture 5)",
+    CSE4303_L6: "Introduction to Computer Security (Lecture 6)",
+    CSE4303_L7: "Introduction to Computer Security (Lecture 7)",
 }
