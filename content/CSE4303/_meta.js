0e28ba6261758b01c27c2a4ee8b4085d624df8ae
Trance-0 <60459821+Trance-0@users.noreply.github.com>
2026-02-12 11:56:24 -0600

    update

--- content/CSE4303/_meta.js
+++ content/CSE4303/_meta.js
@@ -10,4 +10,6 @@ export default {
     CSE4303_L5: "Introduction to Computer Security (Lecture 5)",
     CSE4303_L6: "Introduction to Computer Security (Lecture 6)",
     CSE4303_L7: "Introduction to Computer Security (Lecture 7)",
+    CSE4303_L8: "Introduction to Computer Security (Lecture 8)",
+    CSE4303_L9: "Introduction to Computer Security (Lecture 9)",
 }
